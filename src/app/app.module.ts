77a30dff9895d89256dc694b3ce9cf84da1d68d8
AlexSam10 <zalexis864@gmail.com>
2022-11-05 11:45:59 -0600

    resistencias

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ResistenciaComponent } from './resistencia/resistencia.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResistenciaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
